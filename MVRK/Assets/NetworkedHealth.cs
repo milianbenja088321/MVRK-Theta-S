19f2b847d04bbedc84abf64c84124f2ff7684a79
milianbenja088321 <vinyelortiz@gmail.com>
2018-06-27 09:33:59 -0400

    Now I Added the NetworkedHealth.cs

---
+++ MVRK/Assets/NetworkedHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkedHealth : NetworkBehaviour
+{
+    private const int mMaxHealth = 100;
+
+    
+    [SyncVar]
+    public int mHealth = mMaxHealth;
+    
+   public void TakeDamage(int amount)
+    {
+        if (!isServer) return;
+
+        mHealth -= amount;
+
+        if (mHealth <= 0)
+        {
+            mHealth = 0;
+            Debug.Log("Dead");
+       }
+
+    }
+
+    private void OnChangeHealth(int _currentHealth)
+    {
+        Debug.Log("OnChangeHealth()::");
+    }
+}
