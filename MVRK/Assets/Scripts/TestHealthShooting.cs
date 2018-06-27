dd70e35877703d5b3045eee0170920a9e017bc88
Bruno Franco <brunothebeast@gmail.com>
2018-06-27 15:31:34 -0400

    lkjgdasfl;k

---
+++ MVRK/Assets/Scripts/TestHealthShooting.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestHealthShooting : MonoBehaviour
+{
+    [SerializeField] private GameObject projObject = null;
+    [SerializeField] private Transform projectileSpawn;
+
+    private Projectile theBullet = null;
+    private GameObject projectile;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shoot();
+        }
+    }
+
+    public void shoot()
+    {
+        Debug.Log("RpcShoot:: Called in the network");
+
+        projectile = Instantiate(
+            projObject,
+            projectileSpawn.position,
+            projectileSpawn.rotation
+            );
+
+        theBullet = projectile.GetComponent<Projectile>();
+        theBullet.projectileID++;
+
+        Debug.Log("CmdShoot:: Projectile ID: " + theBullet.projectileID);
+
+        projectile.GetComponent<Rigidbody>().velocity = projectile.transform.forward * 6;
+        projectile.transform.SetParent(null);
+
+      //  Instantiate(projectile);
+
+        // Call RPC for because this command only wokrs for server.
+
+        Destroy(projectile, 2.0f);
+    }
+}
